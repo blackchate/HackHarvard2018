5a9740c904ed97e9a44120318a6534c50cc2177e
Alex Lo <hongtzang@gmail.com>
2018-10-20 02:42:32 -0400

    Xcode project

---
+++ autoBold/autoBold/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autoBold
+//
+//  Created by Furaha Damien on 2018-10-20.
+//  Copyright © 2018 curtesy of damien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
