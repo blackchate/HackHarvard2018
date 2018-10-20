168dc1713897ed529992f7d7ee65f49766fa622d
Furaha <furahadamien30@gmail.com>
2018-10-20 05:22:56 -0400

    maps added

--- autoBold/autoBold/AppDelegate.swift
+++ autoBold/autoBold/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreData
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyA0ATWgDeMMGWTPGErBZwB_Ix6lVIcPygU")
+        GMSPlacesClient.provideAPIKey("AIzaSyA0ATWgDeMMGWTPGErBZwB_Ix6lVIcPygU")
         return true
     }
 
